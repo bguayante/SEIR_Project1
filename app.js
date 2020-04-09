afef812766a99f4eb9db3ff10186bd2c7e4a385c
brian-guayante <brian-guayante@gmail.com>
2020-04-09 09:16:31 -0700

    Files created

---
+++ app.js
@@ -0,0 +1,9 @@
+const button1 = document.querySelector('.button1')
+const button2 = document.querySelector('.button2')
+
+function clickButton(event) {
+    alert("Click 'OK' to download viruses. Dummy.")
+}    
+
+button1.addEventListener('click', clickButton)
+button2.addEventListener('click', clickButton);
