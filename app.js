cf34eb0822b6f7365ff5358cac5ca41ef60b54c8
brian-guayante <brian-guayante@gmail.com>
2020-04-13 16:30:39 -0700

    bugs squashed

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 
 let flashCards = [
 	{
-		question: 'Q1',
-		answer: 'A1',
+		question: 'What is Doc Antle a doctor in?',
+		answer: '',
 	},
 	{
 		question: 'Q2',
@@ -58,9 +58,8 @@ document.body.onkeyup = function (event) {
 	} else if (event.key === ' ' && prompt.innerHTML != congrats) {
 		prompt.innerHTML = flashCards[index - 1].answer;
 	} else if ((prompt.innerHTML = congrats)) {
-		prompt.innerHTML = flashCards[index].question;
+		return;
 	}
-	console.log('index:', index);
 };
 
 /////////////////////////////////
@@ -72,7 +71,7 @@ function showQuestion() {
 	if (flashCards.length <= 1) {
 		prompt.innerHTML = congrats;
 		//Reset index counter to 0 when end of array is reached
-	} else if (index == flashCards.length) {
+	} else if (index == flashCards.length - 1) {
 		index = 0;
 		prompt.innerHTML = flashCards[0].question;
 		index += 1;
@@ -87,9 +86,6 @@ function showQuestion() {
 			: (prompt.innerHTML = flashCards[index].question);
 		index += 1;
 	}
-	console.log('index:', index);
-	console.log('flashcards length:', flashCards.length);
-	console.log(flashCards);
 }
 
 //////////////////
@@ -97,51 +93,19 @@ function showQuestion() {
 //////////////////
 
 function rightClickHandler() {
-	if (flashCards.length <= 0) {
+	if (flashCards.length == 1) {
 		prompt.innerHTML = congrats;
+		if (flashCards.length) {
+			flashCards.splice(index, 1);
+		}
 	} else if (index == flashCards.length - 1) {
 		index = 0;
-		// } else if ((index == 0) && (flashCards.length == 1)){
-		// 	prompt.innerHTML = 'Nice job!';
-		// 	flashCards.pop();
-		// 	console.log('POP')
-		//	index += 1
-	} else if (flashCards.length == 1) {
-		prompt.innerHTML = 'Nice job!';
-		flashCards.splice(-1, 1);
-		console.log('POP');
 	} else {
 		prompt.innerHTML = 'Nice job!';
 		flashCards.splice(index - 1, 1);
-		console.log('index:', index);
-		console.log('flashcards length:', flashCards.length);
-		console.log(flashCards);
 	}
 }
+
 //Set event listeners
 promptClick.addEventListener('click', showQuestion);
 rightButton.addEventListener('click', rightClickHandler);
-
-// let cardID = Math.floor(Math.random() * flashCards.length);
-// function rightClickHandler() {
-// 	prompt.innerHTML = flashCards[cardID].question;
-// 	flashCards.splice(cardID, 1);
-// 	// cardID = Math.floor(Math.random() * flashCards.length);
-// 	console.log(flashCards);
-// 	console.log(cardID)
-// }
-
-// function promptClickHandler(){
-// 	prompt.innerHTML = flashCards[cardID].answer ;
-// }
-
-// rightButton.addEventListener('click', rightClickHandler);
-// promptClick.addEventListener('click', promptClickHandler)
-
-//Alternate questions and answers
-//     counter += 1
-//     if (counter % 2 === 0) {
-//         //Replace <p> text with questions and answers
-//
-//     } else if (counter % 2 === 1)
-//         prompt.innerHTML = flashCards[cardID].question;
